e41061607fdb554d0c9c2fdeeab1dfcdaf8b50f0
Joshua Turingan <turingan.joshua@gmail.com>
2023-11-14 17:29:52 +0800

    Update book page

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -5,6 +5,8 @@ import { fetchBook } from '@/api/books'
 
 import BookActions from '@/components/BookActions'
 
+import { ArrowLeftIcon } from '@heroicons/react/24/solid'
+
 type Props = {
   params: {
     id: string
@@ -16,18 +18,23 @@ const BookSummary = async ({ params: { id } }: Props) => {
 
   return (
     <div className='flex flex-col p-4'>
-      <div className='relative w-100 mb-2'>
+      <ArrowLeftIcon className='text-gray-500 h-8 w-8 mb-2' />
+      <div className='relative w-1/2 mb-2'>
         <Image
           src={data.volumeInfo.imageLinks.medium}
-          width={128}
-          height={161}
+          width={575}
+          height={684}
+          sizes='100%'
           alt={`Book Preview of ${data.title}`}
         />
       </div>
       <h2 className='text-2xl font-medium mb-2'>{data.volumeInfo.title}</h2>
-      <div className='description mb-2'>{parse(data.volumeInfo.description)}</div>
-      <p className='mb-2'>
-        {data.volumeInfo.publishedDate} - {data.volumeInfo.authors.join(', ')}
+      <div className='description mb-2'>
+        {parse(data.volumeInfo.description)}
+      </div>
+      <p className='flex flex-col mb-2'>
+        <span>Publish Year: {new Date(data.volumeInfo.publishedDate).getFullYear()}</span>
+        <span>Author/s: {data.volumeInfo.authors.join(', ')}</span>
       </p>
       <BookActions id={id} status='fee' />
     </div>
